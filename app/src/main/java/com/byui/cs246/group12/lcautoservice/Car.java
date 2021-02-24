536bcee85c44a4ea4f4a99af178e4ea0b02f3c8c
Victor <victor.hugooo@hotmail.com.br>
2021-02-24 20:27:10 -0300

    Created a Car class for tests

---
+++ app/src/main/java/com/byui/cs246/group12/lcautoservice/Car.java
@@ -0,0 +1,47 @@
+package com.byui.cs246.group12.lcautoservice;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int year;
+    private int kilometers;
+
+    public Car(String brand, String model, int year, int kilometers) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.kilometers = kilometers;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getKilometers() {
+        return kilometers;
+    }
+
+    public void setKilometers(int kilometers) {
+        this.kilometers = kilometers;
+    }
+}
