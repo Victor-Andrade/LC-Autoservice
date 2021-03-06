fc2eeefb376fbbb85eb0c46356cbe3ea33ff9f6c
Victor <victor.hugooo@hotmail.com.br>
2021-03-05 22:42:34 -0300

    Created Unit tests for CarRepository and ExcelManager

--- app/src/main/java/com/byui/cs246/group12/lcautoservice/model/ExcelManager.java
+++ app/src/main/java/com/byui/cs246/group12/lcautoservice/model/ExcelManager.java
@@ -1,11 +1,10 @@
 package com.byui.cs246.group12.lcautoservice.model;
 
-import com.byui.cs246.group12.lcautoservice.model.Car;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class ExcelManager {
     public List<String> getProcedures(Car car){
-        return null;
+        return new ArrayList<>();
     }
 }
