56aa064f0f8bdbcd59e2c82a2d92296aa13be29c
Victor <victor.hugooo@hotmail.com.br>
2021-03-06 14:08:24 -0300

    Adjusts in Excel reading

--- app/src/main/java/com/byui/cs246/group12/lcautoservice/model/ExcelManager.java
+++ app/src/main/java/com/byui/cs246/group12/lcautoservice/model/ExcelManager.java
@@ -1,5 +1,4 @@
 package com.byui.cs246.group12.lcautoservice.model;
-package com.excelcsv;
 import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
@@ -11,59 +10,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ExcelManager {
-    public List<String> getProcedures(Car car){
+    String filePath;
+
+    public ExcelManager(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public List<String> getProcedures(Car car) {
         return new ArrayList<>();
     }
-}
-public class ExcelReadWrite {
-String filePath;
-ExcelReadWrite(String filePath){
-this.filePath = filePath;
-}
-// writing excel file
-public void writeExcelFile(){
-WritableWorkbook excelSheet = null;
-try {
-excelSheet = Workbook.createWorkbook(new File(filePath));
-WritableSheet excelFile = myCar.createSheet("Sheet 1", 0);
-Label label = new Label(0, 0, "Test Count");
-excelFile.addCell(label);
-Number number = new Number(0, 1, 1);
-excelFile.addCell(number);
-label = new Label(1, 0, "Result");
-excelFile.addCell(label);
-label = new Label(1, 1, "Passed");
-excelFile.addCell(label);
-number = new Number(0, 2, 2);
-excelFile.addCell(number);
-label = new Label(1, 2, "Passed 2");
-excelFile.addCell(label);
-excelSheet.write();
-}catch (Exception e){
-e.printStackTrace();
-}
-}
-// reading excel file
-public void readExcelFile(){
-try {
-excelSheet = Workbook.getWorkbook(new File(filePath));
-Sheet sheet = workbook.getSheet(0);
-Cell cell1 = sheet.getCell(0, 0);
-System.out.print(cell1.getContents() + ":");
-Cell cell2 = sheet.getCell(0, 1);
-System.out.println(cell2.getContents());
-Cell cell3 = sheet.getCell(1, 0);
-System.out.print(cell3.getContents() + ":");
-Cell cell4 = sheet.getCell(1, 1);
-System.out.println(cell4.getContents());
-System.out.print(cell1.getContents() + ":");
-cell2 = sheet.getCell(0, 2);
-System.out.println(cell2.getContents());
-System.out.print(cell3.getContents() + ":");
-cell4 = sheet.getCell(1, 2);
-System.out.println(cell4.getContents());
-} catch (IOException e) {
-e.printStackTrace();
-}
-}
+
+    // reading excel file
+    public void readExcelFile() throws IOException, BiffException {
+        Workbook excelSheet = Workbook.getWorkbook(new File(filePath));
+        Sheet sheet = excelSheet.getSheet(0);
+        Cell cell1 = sheet.getCell(0, 0);
+        System.out.print(cell1.getContents() + ":");
+        Cell cell2 = sheet.getCell(0, 1);
+        System.out.println(cell2.getContents());
+        Cell cell3 = sheet.getCell(1, 0);
+        System.out.print(cell3.getContents() + ":");
+        Cell cell4 = sheet.getCell(1, 1);
+        System.out.println(cell4.getContents());
+        System.out.print(cell1.getContents() + ":");
+        cell2 = sheet.getCell(0, 2);
+        System.out.println(cell2.getContents());
+        System.out.print(cell3.getContents() + ":");
+        cell4 = sheet.getCell(1, 2);
+        System.out.println(cell4.getContents());
+    }
 }
