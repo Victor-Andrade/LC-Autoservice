10cbed6f37fba8a0d20d1e023e632b95684c7483
DiegoVargas <var20010@byui.edu>
2021-03-11 22:53:23 -0600

    Added spinners and tried to setup the set on item selected listener

--- app/src/main/java/com/byui/cs246/group12/lcautoservice/CarInfoActivity.java
+++ app/src/main/java/com/byui/cs246/group12/lcautoservice/CarInfoActivity.java
@@ -11,14 +11,12 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
 import java.util.ArrayList;
-import java.util.List;
 
 public class CarInfoActivity extends AppCompatActivity {
     private static final String TAG = "CarInfoActivity";
     public static final String SHARED_PREF_NAME = "autoInfo";
-    Spinner tradeMarkSpinner;
+    Spinner tradeMarkSpinner, modelSpinner, yearSpinner, kilometersSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,30 +30,73 @@ public class CarInfoActivity extends AppCompatActivity {
     private void startSpinners() {
         //Test list
         //This list will come from the Excel class
-        List<String> list = new ArrayList<>();
-        list.add("Honda");
-        list.add("Fiat");
+        ArrayList<String> trademarkList = new ArrayList<>();
+        trademarkList.add("Honda");
+        trademarkList.add("Mitsubishi");
 
-        //This is the 1º spinner
+        ArrayList<String> modelList = new ArrayList<>();
+        modelList.add("City");
+        modelList.add("L200");
+
+        ArrayList<String> yearsList = new ArrayList<>();
+        yearsList.add("2019");
+        yearsList.add("2020");
+        yearsList.add("2021");
+
+        ArrayList<String> kilometersList = new ArrayList<>();
+        kilometersList.add("5");
+        kilometersList.add("10");
+        kilometersList.add("20");
+        kilometersList.add("30");
+        kilometersList.add("40");
+        kilometersList.add("50");
+
+
+        //This is the 1st spinner
         tradeMarkSpinner = findViewById(R.id.tradeMarkSpinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_item, list);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        tradeMarkSpinner.setAdapter(adapter);
+        ArrayAdapter<String> arrayAdapter_parent = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, trademarkList);
+        arrayAdapter_parent.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        tradeMarkSpinner.setAdapter(arrayAdapter_parent);
+
+        //This is the 2nd spinner
+        modelSpinner = findViewById(R.id.modelSpinner);
+        final ArrayAdapter[] arrayAdapter_model = new ArrayAdapter[]{new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, modelList)};
+        arrayAdapter_model[0].setDropDownViewResource(android.R.layout.simple_spinner_item);
+        modelSpinner.setAdapter(arrayAdapter_model[0]);
+
+        //This is the 3rd spinner
+        yearSpinner = findViewById(R.id.yearSpinner);
+        ArrayAdapter<String> arrayAdapter_years = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, yearsList);
+        arrayAdapter_years.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        yearSpinner.setAdapter(arrayAdapter_years);
+
+        //This is the 4th spinner
+        kilometersSpinner = findViewById(R.id.kilometersSpinner);
+        ArrayAdapter<String> arrayAdapter_kilometers = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, kilometersList);
+        arrayAdapter_kilometers.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        kilometersSpinner.setAdapter(arrayAdapter_kilometers);
+
         tradeMarkSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 //When select something, we want to search on the database to check the list of the
                 // models from this brand.
                 //Then we want to update the content for the next spinner
-
                 String item = parent.getItemAtPosition(position).toString();
+                if(item.equals("Honda")) {
+                    arrayAdapter_model[0] =new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,modelList);
+                }
+                modelSpinner.setAdapter(arrayAdapter_model[0]);
+
                 Log.i(TAG, "EU Spinner selected " + item);
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
-
             }
         });
     }
