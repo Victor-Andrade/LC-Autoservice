825f74b69019aa8fa353c42b56264e33b2e21414
Victor <victor.hugooo@hotmail.com.br>
2021-03-17 21:32:19 -0300

    Added documentation for Car and CarRepository

--- app/src/main/java/com/byui/cs246/group12/lcautoservice/model/Car.java
+++ app/src/main/java/com/byui/cs246/group12/lcautoservice/model/Car.java
@@ -1,11 +1,21 @@
 package com.byui.cs246.group12.lcautoservice.model;
 
+/**
+ * This class represent the car info
+ */
 public class Car {
     private String brand;
     private String model;
     private int year;
     private int kilometers;
 
+    /**
+     *
+     * @param brand Represents the car brand.
+     * @param model Represents the car model.
+     * @param year Represents the car year.
+     * @param kilometers Represents the car kilometers.
+     */
     public Car(String brand, String model, int year, int kilometers) {
         this.brand = brand;
         this.model = model;
@@ -13,34 +23,66 @@ public class Car {
         this.kilometers = kilometers;
     }
 
+    /**
+     *
+     * @return The brand of the car.
+     */
     public String getBrand() {
         return brand;
     }
 
+    /**
+     *
+     * @param brand The brand of the car.
+     */
     public void setBrand(String brand) {
         this.brand = brand;
     }
 
+    /**
+     *
+     * @return The car model.
+     */
     public String getModel() {
         return model;
     }
 
+    /**
+     *
+     * @param model The model of the car.
+     */
     public void setModel(String model) {
         this.model = model;
     }
 
+    /**
+     *
+     * @return The car year.
+     */
     public int getYear() {
         return year;
     }
 
+    /**
+     *
+     * @param year The year of the car assembly.
+     */
     public void setYear(int year) {
         this.year = year;
     }
 
+    /**
+     *
+     * @return Return how many kilometers the car has been used.
+     */
     public int getKilometers() {
         return kilometers;
     }
 
+    /**
+     *
+     * @param kilometers Set how many kilometers the car has been used.
+     */
     public void setKilometers(int kilometers) {
         this.kilometers = kilometers;
     }
--- app/src/main/java/com/byui/cs246/group12/lcautoservice/model/CarRepository.java
+++ app/src/main/java/com/byui/cs246/group12/lcautoservice/model/CarRepository.java
@@ -3,26 +3,54 @@ package com.byui.cs246.group12.lcautoservice.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * CarRepository abstracts and handle the readings from the  {@link ExcelManager ExcelManager}.
+ */
 public class CarRepository {
-
     private ExcelManager manager;
 
+    /**
+     *
+     * @return Return a list of all the brands in the repository.
+     */
     public List<String> getBrands(){
         return new ArrayList<>();
     }
 
+    /**
+     * This handles the model solicitations.
+     * @param brand The brand that you want the list of models.
+     * @return Return the list of the models based on the received brand.
+     */
     public List<String> getModels(String brand){
         return new ArrayList<>();
     }
 
+    /**
+     *
+     * @param brand The brand of the car.
+     * @param model The model of the car.
+     * @return Return the years that the app has in catalog.
+     */
     public List<String> getYears(String brand, String model){
         return new ArrayList<>();
     }
 
-    public List<String> getKilometers(String model, String year) {
+    /**
+     * @param brand The brand of the car.
+     * @param model The model of the car.
+     * @param year The year of the car.
+     * @return Returns list of kilometers that the car needs to be checked
+     */
+    public List<String> getKilometers(String brand, String model, String year) {
         return new ArrayList<>();
     }
 
+    /**
+     * List the procedures needed for {@link Car Car}.
+     * @param car The {@link Car Car}. to check the procedures.
+     * @return Return the list of procedures for the received {@link Car Car}.
+     */
     private List<String> getProcedures(Car car) {
         return null;
     }
